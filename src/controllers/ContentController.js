af9744209de0cc090740fec364872ea2bc7ee48c
deniev <paul5555@bk.ru>
2020-08-18 23:11:22 +0300

    init

--- src/controllers/ContentController.js
+++ src/controllers/ContentController.js
@@ -5,11 +5,18 @@ class ContentController
     static async get(req, res)
     {
         try {
-            const [folders] = await Promise.all([
-                Folder.getByUser("3434")
+            const [
+                folders,
+                messages
+            ] = await Promise.all([
+                Folder.getByUser("3434"),
+                Folder.getByUser("454545"),
             ]);
 
-            return res.json({ folders });
+            return res.json({
+                folders,
+                messages
+            });
         } catch (e) {
 
         }
